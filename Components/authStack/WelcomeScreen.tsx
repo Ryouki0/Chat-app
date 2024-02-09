22039c3217331fd7711fae2f8740a5018da5e242
BuildTools <unconfigured@null.spigotmc.org>
2024-02-09 18:21:56 +0100

    .

--- Components/authStack/WelcomeScreen.tsx
+++ Components/authStack/WelcomeScreen.tsx
@@ -4,7 +4,6 @@ import React, { useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { ThemeContext, Theme } from '../../hooks/useTheme';
-import { sendPushNotification } from '../../notification';
 export default function WelcomeScreen({route, navigation}) {
 	const theme = useContext<Theme>(ThemeContext);
 	console.log('theme: ', theme);
@@ -16,9 +15,6 @@ export default function WelcomeScreen({route, navigation}) {
 			}}></Button>
 			<Button title='Sign up' onPress={() => {
 				navigation.navigate('CreateAccountScreen');
-			}}></Button>
-			<Button title='sendPushNotification' onPress={() => {
-				sendPushNotification('ExponentPushToken[lODtqvOFLd0I5Hf5pAZqbH]');
 			}}></Button>	
 		</View>
 	);
